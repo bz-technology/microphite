a1d18e8e030944bf97474c90be1ccded69d156f1
Bob Ziuchkovski <bob.ziuchkovski@gmail.com>
2013-11-18 19:13:46 -0700

    Flesh-out tests

--- spec/client/socket_spec.rb
+++ spec/client/socket_spec.rb
@@ -13,9 +13,50 @@ module Microphite
     end
 
     describe 'write' do
-      it 'should correctly format a single metric' do
-        @client.write(key: 0)
-        @server.bytes.should match(/^key 0 \d+\n$/)
+      it 'should support symbol and string metrics' do
+        @client.write(key1: 1)
+        @client.write(key2: 2, key3: 3)
+        @client.write('key4' => 4)
+        @client.write('key5' => 5, 'key6' => 6)
+        @client.write('key7' => 7, :key8 => 8)
+        @client.write(:key9 => 9, 'key10' => 10)
+        @client.shutdown
+
+        lines = @server.bytes
+        (1..10).each do |n|
+          lines.should match(/^key#{n} #{n} \d+$/)
+        end
+      end
+    end
+
+    describe 'gather' do
+      it 'should support symbol and string metrics' do
+        @client.gather(key1: 1)
+        @client.gather(key2: 2, key3: 3)
+        @client.gather('key4' => 4)
+        @client.gather('key5' => 5, 'key6' => 6)
+        @client.gather('key7' => 7, :key8 => 8)
+        @client.gather(:key9 => 9, 'key10' => 10)
+        @client.shutdown
+
+        lines = @server.bytes
+        (1..10).each do |n|
+          lines.should match(/^key#{n} #{n} \d+$/)
+        end
+      end
+
+      it 'should accumulate data' do
+        (1..10).each do
+          @client.gather(key1: 1)
+          @client.gather('key2' => 2)
+          @client.gather(key3 => 3, 'key4' => 4)
+        end
+        @client.shutdown
+
+        lines = @server.bytes
+        (1..10).each do |n|
+          lines.should match(/^key#{n} #{n * 10} \d+$/)
+        end
       end
     end
   end
--- spec/support/client_shared_examples.rb
+++ spec/support/client_shared_examples.rb
@@ -1,9 +1,11 @@
 # Copyright (c) 2013 BZ Technology Services, LLC
 # Released under the MIT License (http://opensource.org/licenses/MIT)
 
+PUBLIC_METHODS = [:write, :gather, :prefix, :every, :shutdown]
+
 shared_examples 'microphite client' do |client|
   describe 'interface' do
-    [:write, :gather, :prefix, :every, :shutdown].each do |method|
+    PUBLIC_METHODS.each do |method|
       it 'should respond to ' + method.to_s do
         client.should respond_to method
       end
@@ -37,6 +39,27 @@ shared_examples 'microphite client' do |client|
       expect { client.gather(0) }.not_to raise_error
       expect { client.gather(key: 'string') }.not_to raise_error
       expect { client.gather(key: nil) }.not_to raise_error
+    end
+  end
+
+  describe :prefix do
+    it 'should return a client-like object' do
+      prefixed = client.prefix 'test'
+      PUBLIC_METHODS.each do |method|
+        prefixed.should respond_to method
+      end
+    end
+  end
+
+  describe :every do
+    it 'should not blow up' do
+      expect { client.every(1) {true} }.not_to raise_error
+    end
+  end
+
+  describe :shutdown do
+    it 'should not blow up' do
+      expect { client.shutdown }.not_to raise_error
     end
   end
 end
