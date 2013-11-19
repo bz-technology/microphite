20423a4cd241052b2aab60279a845d52c637f4e7
Bob Ziuchkovski <bob.ziuchkovski@gmail.com>
2013-11-18 22:45:08 -0700

    WIP: Towards writing an async client, but very rudimentary

--- lib/microphite/client/socket.rb
+++ lib/microphite/client/socket.rb
@@ -2,45 +2,86 @@
 # Released under the MIT License (http://opensource.org/licenses/MIT)
 
 require 'socket'
+require 'thread'
 
 module Microphite
   module Client
     class Socket < Base
-      def initialize(host, port=2003, transport=:udp)
-        @host = host
-        @port = port
-        @transport = transport
+      def initialize(options)
+        defaults = {
+            port: 2003,           # Server port
+            transport: :udp,      # Transport: :tcp or :udp
+        }
+        params = defaults.merge(options)
+
+        raise(ArgumentError, ':host option missing') unless params.has_key? :host
+
+        # Read-only state
+        @host = params[:host]
+        @port = params[:port]
+        @prefix = params[:prefix]
+        @transport = params[:transport]
+
+        @gather_stack = []
+        @write_stack = []
+        @lock = Mutex.new
+
+        @thread = Thread.new do
+          worker_loop
+        end
       end
 
       def write(metrics)
-        if metrics.is_a? Hash
-          begin
-            ensure_connected
-            metrics.each_pair do |key, value|
-              @socket.send("#{key} #{value} #{Time.now.to_i}\n", 0)
-            end
-          rescue
-          end
-        end
+        push(@write_stack, Timestamped.new(metrics.clone)) if metrics.is_a? Hash
+        true
       end
 
       def gather(metrics)
-        if metrics.is_a? Hash
+        push(@gather_stack, Timestamped.new(metrics.clone)) if metrics.is_a? Hash
+        true
+      end
+
+      def close(timeout=nil)
+        # FIXME
+        @socket.close unless @socket.nil?
+        @thread.kill if @thread.alive?
+        true
+      end
+
+      private
+
+      def worker_loop
+        loop do
           begin
+            writes = nil
+            gathers = nil
+
+            @lock.synchronize do
+              # Swap existing stacks with new ones
+              writes, @write_stack = @write_stack, []
+              gathers, @gather_stack = @gather_stack, []
+            end
+
             ensure_connected
-            metrics.each_pair do |key, value|
-              @socket.send("#{key} #{value} #{Time.now.to_i}\n", 0)
+            [writes, gathers].each do |stack|
+              stack.each do |metrics|
+                metrics.each_pair do |key, value|
+                  @socket.send("#{key} #{value} #{Time.now.to_i}\n", 0)
+                end
+              end
             end
           rescue
+            # Protection from garbage input
           end
         end
       end
 
-      def close(timeout=nil)
-        @socket.close unless @socket.nil?
+      def push(stack, value)
+        @lock.synchronize do
+          stack << value
+          pushed = true
+        end
       end
-
-      private
 
       def ensure_connected
         if @socket.nil?
@@ -53,6 +94,16 @@ module Microphite
             else
               raise(ArgumentError, "transport type is invalid: #{@transport}")
           end
+        end
+      end
+
+
+      class Timestamped
+        attr_accessor :value, :time
+
+        def initialize(value)
+          @value = value
+          @time = Time.now.to_i
         end
       end
     end
--- spec/client/socket_spec.rb
+++ spec/client/socket_spec.rb
@@ -5,11 +5,11 @@ require 'spec_helper'
 
 module Microphite
   shared_examples 'microphite socket client' do |transport|
-    include_examples 'microphite client', Client::Socket.new('localhost', 2003, transport)
+    include_examples 'microphite client', Client::Socket.new(host: 'localhost', port: 2003, transport: transport)
 
     before do
       @server = Helpers::SingleServe.new(transport)
-      @client = Client::Socket.new('localhost', @server.port, transport)
+      @client = Client::Socket.new(host:'localhost', port:@server.port, transport:transport)
     end
 
     describe 'write' do
@@ -49,7 +49,7 @@ module Microphite
         (1..10).each do
           @client.gather(key1: 1)
           @client.gather('key2' => 2)
-          @client.gather(key3 => 3, 'key4' => 4)
+          @client.gather(:key3 => 3, 'key4' => 4)
         end
         @client.close
 
