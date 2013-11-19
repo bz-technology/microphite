c6d119e0cdadd67d8965aab9c1c6eaca8453b1b1
Bob Ziuchkovski <bob.ziuchkovski@gmail.com>
2013-11-19 01:33:55 -0700

    Finish initial implementation of Client::Socket

--- lib/microphite/client/socket.rb
+++ lib/microphite/client/socket.rb
@@ -10,7 +10,12 @@ module Microphite
       def initialize(options)
         defaults = {
             port: 2003,           # Server port
-            transport: :udp,      # Transport: :tcp or :udp
+            prefix: '',           # Prefix every key with this
+            transport: :udp,      # Transport: tcp or udp
+            flush_interval: 10,   # How often to flush gathered metrics
+            limit: 1000000,       # Size limit for gather and write stacks
+            min_delay: 2,         # Initial delay for write failures (seconds)
+            max_delay: 60         # Maximum delay for write failures (seconds)
         }
         params = defaults.merge(options)
 
@@ -19,36 +24,72 @@ module Microphite
         # Read-only state
         @host = params[:host]
         @port = params[:port]
-        @prefix = params[:prefix]
+        @prefix = params[:prefix] unless params[:prefix].nil?
         @transport = params[:transport]
+        @flush_interval = params[:flush_interval]
+        @limit = params[:limit]
+        @min_delay = params[:min_delay]
+        @max_delay = params[:max_delay]
 
+        # Shared state
+        @status = :running
         @gather_stack = []
         @write_stack = []
-        @lock = Mutex.new
 
-        @thread = Thread.new do
+        # Worker state
+        @accumulating = {}
+        @next_flush = Time.now.to_i + @flush_interval
+        @socket = nil
+
+        # Synchronization primitives
+        @lock = Mutex.new
+        @worker_event = ConditionVariable.new   # Signals worker to wake-up
+        @shutdown_event = ConditionVariable.new # Signals close() caller when shutdown is complete
+
+        # The worker thread does all of the data processing and socket writing
+        Thread.new do
           worker_loop
         end
       end
 
       def write(metrics)
         push(@write_stack, Timestamped.new(metrics.clone)) if metrics.is_a? Hash
-        true
       end
 
       def gather(metrics)
         push(@gather_stack, Timestamped.new(metrics.clone)) if metrics.is_a? Hash
-        true
       end
 
       def close(timeout=nil)
-        # FIXME
-        @socket.close unless @socket.nil?
-        @thread.kill if @thread.alive?
-        true
+        @lock.synchronize do
+          case @status
+            when :running, :ending
+              @status = :ending
+              @worker_event.signal
+              @shutdown_event.wait(@lock, timeout)
+              return @status == :shutdown
+            when :shutdown
+              return true
+            else
+              # TODO: error callback
+          end
+        end
       end
 
+
       private
+
+      def push(stack, value)
+        pushed = false
+        @lock.synchronize do
+          if stack.length <= @limit and @status == :running
+            stack << value
+            pushed = true
+            @worker_event.signal
+          end
+        end
+        pushed
+      end
 
       def worker_loop
         loop do
@@ -57,46 +98,115 @@ module Microphite
             gathers = nil
 
             @lock.synchronize do
+              if @write_stack.empty? and @gather_stack.empty?
+                case @status
+                  when :running
+                    wait_time = @next_flush - Time.now.to_i
+                    if wait_time > 0
+                      @worker_event.wait(@lock, wait_time)
+                    end
+
+                  when :ending, :shutdown
+                    flush_accumulating
+
+                    # Try to be nice by closing the socket, but don't fall into a retry loop if it fails
+                    socket, @socket = @socket, nil
+                    socket.close unless socket.nil?
+                    @status = :shutdown
+                    @shutdown_event.broadcast
+                    Thread.exit
+                  else
+                    # TODO: Error callback
+                end
+              end
+
               # Swap existing stacks with new ones
               writes, @write_stack = @write_stack, []
               gathers, @gather_stack = @gather_stack, []
             end
 
-            ensure_connected
-            [writes, gathers].each do |stack|
-              stack.each do |metrics|
-                metrics.each_pair do |key, value|
-                  @socket.send("#{key} #{value} #{Time.now.to_i}\n", 0)
-                end
-              end
+            process_writes writes
+            process_gathers gathers
+
+            # TODO: Use helper method
+            now = Time.now.to_i
+            if now > @next_flush
+              flush_accumulating
             end
-          rescue
-            # Protection from garbage input
+
+          rescue Exception => e
+            # TODO: Error callback
           end
         end
       end
 
-      def push(stack, value)
-        @lock.synchronize do
-          stack << value
-          pushed = true
+      def process_writes(writes)
+        writes.each do |timestamped|
+          timestamped.value.each_pair do |k, v|
+            write_line(k, v, timestamped.time)
+          end
         end
       end
 
-      def ensure_connected
-        if @socket.nil?
-          case @transport
-            when :tcp
-              @socket = TCPSocket.new(@host, @port)
-            when :udp
-              @socket = UDPSocket.new
-              @socket.connect(@host, @port)
+      def process_gathers(gathers)
+        gathers.each do |timestamped|
+          timestamped.value.each_pair do |k, v|
+            next if k.nil? or v.nil?
+            key = k.to_sym
+            if @accumulating.has_key? key
+              @accumulating[key] += v
             else
-              raise(ArgumentError, "transport type is invalid: #{@transport}")
+              @accumulating[key] = v
+            end
           end
         end
       end
 
+      def flush_accumulating
+        now = Time.now.to_i
+        @accumulating.each_pair { |k, v| write_line(k, v, now) }
+        @accumulating.clear
+        @next_flush = now + @flush_interval
+      end
+
+      def write_line(key, value, time)
+        return unless key.is_a? String or key.is_a? Symbol
+        return unless value.is_a? Fixnum
+        return unless time.is_a? Fixnum
+
+        sent = false
+        failure_delay = @min_delay
+        until sent
+          begin
+            if @socket.nil?
+              new_socket
+            end
+            @socket.send("#{@prefix}#{key} #{value} #{time}\n", 0)
+            sent = true
+
+          rescue
+            # Protection from network failure
+            # TODO: Error callback
+            sleep failure_delay
+            new_socket
+            if failure_delay < @max_delay
+              failure_delay += 1
+            end
+          end
+        end
+      end
+
+      def new_socket
+        case @transport
+          when :tcp
+            @socket = TCPSocket.new(@host, @port)
+          when :udp
+            @socket = UDPSocket.new
+            @socket.connect(@host, @port)
+          else
+            # TODO: Error callback
+        end
+      end
 
       class Timestamped
         attr_accessor :value, :time
