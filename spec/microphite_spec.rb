169542ed952f782a06235cab7e75f0adba1ceded
Bob Ziuchkovski <bob.ziuchkovski@gmail.com>
2013-11-23 12:13:21 -0700

    Fix module convenience method coverage

--- spec/microphite_spec.rb
+++ spec/microphite_spec.rb
@@ -1,14 +1,14 @@
 # Sanity tests for the convenience factories
 module Microphite
   describe :client do
-    before_block = Proc.new { @client = Microphite::Client::Socket.new(host: 'localhost') }
+    before_block = Proc.new { @client = Microphite.client(host: 'localhost') }
     after_block = Proc.new {}
 
     it_should_behave_like 'a microphite client', before_block, after_block
   end
 
   describe :noop do
-    before_block = Proc.new { @client = Microphite::Client::Noop.new(host: 'localhost') }
+    before_block = Proc.new { @client = Microphite.noop(host: 'localhost') }
     after_block = Proc.new {}
 
     it_should_behave_like 'a microphite client', before_block, after_block
