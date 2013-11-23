3fc68625b4e65bf4c1fb6d1c4ec0e22fa8487aa9
Bob Ziuchkovski <bob.ziuchkovski@gmail.com>
2013-11-23 12:07:20 -0700

    Fix code coverage metrics

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,18 @@
 # Copyright (c) 2013 BZ Technology Services, LLC
 # Released under the MIT License (http://opensource.org/licenses/MIT)
 
-require 'microphite'
-require 'socket'
-
-# Coveralls support
-require 'coveralls'
-Coveralls.wear!
-
 Dir['./spec/support/**/*.rb'].each { |file| require file }
-
-
 RSpec.configure do |config|
   config.include Helpers
 end
+
+require 'simplecov'
+SimpleCov.configure do
+  add_filter "/spec/"
+end
+
+# Coveralls coverage (simplecov-based)
+require 'coveralls'
+Coveralls.wear!
+
+require 'microphite'
